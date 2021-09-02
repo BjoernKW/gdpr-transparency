10cff74ae06421438539b5ad3bde75fe287c6ee3
BjoernKW <bjoern@bjoernkw.com>
2021-09-02 03:09:40 +0000

    auto commit [ci skip]
    Triggered by commit: https://github.com/BjoernKW/gdpr-transparency/commit/11c04758bc1070668aebcaa4f3b45d8022968af2
    CircleCI build: https://circleci.com/gh/BjoernKW/gdpr-transparency/515

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.3';
+    const SW_VERSION = '12.2.4';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
