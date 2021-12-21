480c5dd9e0603a590ee45b1ca1977eec353a93aa
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-12-21 13:20:13 +0100

    Angular and UI updates

--- src/app/data-import-export/data-import-export.component.ts
+++ src/app/data-import-export/data-import-export.component.ts
@@ -94,16 +94,11 @@ export class DataImportExportComponent implements OnInit {
   }
 
   private static triggerDownload(dataURL: string, filename: string) {
-    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(dataURL, filename);
-      return;
-    } else {
-      const a = window.document.createElement('a');
-      a.href = dataURL;
-      a.download = filename;
-      document.body.appendChild(a);
-      a.click();  // IE: "Access is denied"; see: https://connect.microsoft.com/IE/feedback/details/797361/ie-10-treats-blob-url-as-cross-origin-and-denies-access
-      document.body.removeChild(a);
-    }
+    const a = window.document.createElement('a');
+    a.href = dataURL;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();  // IE: "Access is denied"; see: https://connect.microsoft.com/IE/feedback/details/797361/ie-10-treats-blob-url-as-cross-origin-and-denies-access
+    document.body.removeChild(a);
   }
 }
